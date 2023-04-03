a584fd35d3642a90cc2676eb3bdcce7ca9301168
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-04-03 17:05:00 +0300

    implemented test environment

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,94 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
 import { AppModule } from './../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
+import * as pactum from 'pactum';
+import { FirstLevelPaths, Paths } from '../src/enums';
+import {
+  invalidAuthMock,
+  invalidSignInMock,
+  properAuthMock,
+  properSignInMock,
+} from '../src/prisma/initMocks';
+import { HttpStatus } from '@nestjs/common';
 
-describe('AppController (e2e)', () => {
+const PORT = process.env.PORT;
+
+describe('App (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+
     await app.init();
+    await app.listen(PORT);
+
+    prisma = app.get(PrismaService);
+
+    await prisma.cleanDb();
+
+    pactum.request.setBaseUrl(`http://localhost:${PORT}`);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(() => {
+    app.close();
   });
+
+  describe('Auth', () => {
+    describe('Sign up', () => {
+      it('should sign up', () => {
+        return pactum
+          .spec()
+          .post(`/${FirstLevelPaths.AUTH}/${Paths.SIGN_UP}`)
+          .withBody(properAuthMock)
+          .expectStatus(HttpStatus.CREATED)
+          .inspect();
+      });
+
+      it('should NOT sign up', () => {
+        return pactum
+          .spec()
+          .post(`/${FirstLevelPaths.AUTH}/${Paths.SIGN_UP}`)
+          .withBody(invalidAuthMock)
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+    });
+
+    describe('Sign in', () => {
+      it('should sign in', () => {
+        return pactum
+          .spec()
+          .post(`/${FirstLevelPaths.AUTH}/${Paths.SIGN_IN}`)
+          .withBody(properSignInMock)
+          .expectStatus(HttpStatus.OK)
+          .inspect();
+      });
+
+      it('should NOT sign in', () => {
+        return pactum
+          .spec()
+          .post(`/${FirstLevelPaths.AUTH}/${Paths.SIGN_IN}`)
+          .withBody(invalidSignInMock)
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+    });
+  });
+
+  describe('User', () => {});
+
+  describe('Item', () => {});
+
+  describe('Cart', () => {});
+
+  describe('Comment', () => {});
 });
