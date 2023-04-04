1ba3c74e70cf599843ea83a33c50d5848ffbd8c7
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-04-04 11:20:21 +0300

    extended jwt logic

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+export type PayloadUser = Pick<User, 'id' | 'email' | 'role'>;
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  validate(payload: PayloadUser) {
+    return payload;
+  }
+}
