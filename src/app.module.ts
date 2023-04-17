cd65f0d27bd94fc74320f58c63ce459b8936cafe
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-04-17 15:51:48 +0300

    added default paths for modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,89 @@
 import { Module } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
+
 import { AuthModule } from './auth/auth.module';
+import { BrandModule } from './brand/brand.module';
+import { CartModule } from './cart/cart.module';
+import { CategoryModule } from './category/category.module';
+import { CommentReactionModule } from './comment-reaction/comment-reaction.module';
+import { CommentsModule } from './comments/comments.module';
+import { ItemRatingModule } from './item-rating/item-rating.module';
+import { ItemModule } from './item/item.module';
 import { PrismaModule } from './prisma/prisma.module';
+import {
+  AuthPaths,
+  BrandPaths,
+  CartPaths,
+  CategoryPaths,
+  CommentPaths,
+  ItemPaths,
+  TypePaths,
+  UserPaths,
+} from './shared/types/paths';
+import { TypeModule } from './type/type.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    RouterModule.register([
+      {
+        path: AuthPaths.AUTH,
+        module: AuthModule,
+      },
+      {
+        path: UserPaths.USER,
+        module: UserModule,
+      },
+      {
+        path: BrandPaths.BRAND,
+        module: BrandModule,
+      },
+      {
+        path: CategoryPaths.CATEGORY,
+        module: CategoryModule,
+      },
+      {
+        path: TypePaths.TYPE,
+        module: TypeModule,
+      },
+      {
+        path: ItemPaths.ITEM,
+        module: ItemModule,
+      },
+      {
+        path: CartPaths.CART,
+        module: CartModule,
+      },
+      {
+        path: CommentPaths.COMMENT,
+        module: CommentsModule,
+      },
+      {
+        path: `${ItemPaths.ITEM}/${ItemPaths.RATING}`,
+        module: ItemRatingModule,
+      },
+      {
+        path: `${CommentPaths.COMMENT}/${CommentPaths.REACTION}`,
+        module: CommentReactionModule,
+      },
+    ]),
+    BrandModule,
+    UserModule,
+    CategoryModule,
+    TypeModule,
+    ItemModule,
+    CartModule,
+    CommentsModule,
+    ItemRatingModule,
+    CommentReactionModule,
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  // configure(consumer: MiddlewareConsumer) {
+  //   consumer.apply(RoleMiddleware).forRoutes(BrandPaths.BRAND);
+  // }
+}
