7f272ee5deaa3559e4eefd46b6d00a82827f1b94
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-04-19 21:31:06 +0300

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
 import { config } from 'dotenv';
 
@@ -9,13 +10,28 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   config();
 
+  //enable global pipes
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+  //
 
+  //enable cookie parser
   app.use(cookieParser());
+  //
+
+  //enable swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Internet store application')
+    .setDescription('The Internet store API description')
+    .setVersion('1.0')
+    .addTag('internet-store')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+  //
 
   await app.listen(process.env.PORT);
 }
